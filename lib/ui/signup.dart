cfd302ed85af7a867ea2ca182e8443a9444d9fce
Meg <yusuffrasheed.yr@gmail.com>
2022-05-17 21:59:44 +0100

    reusable flat button created

--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -25,8 +25,8 @@ class _SignUpPageState extends State<SignUpPage> {
                   )
                 ],
               ),
-               const Padding(
-                padding: EdgeInsets.symmetric(vertical: 14),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
                 child: Expanded(
                   child: Text(
                     "See what's happening in the world right now.",
@@ -34,10 +34,71 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ),
               ),
+              Column(
+                children: const [
+                  FlatBtn(
+                    label: 'Continue with Google',
+                    labelColor: Colors.black,
+                    color: Colors.transparent,
+                  ),
+                  FlatBtn(
+                    label: 'Continue with Apple',
+                    labelColor: Colors.black,
+                    color: Colors.transparent,
+                  ),
+                  FlatBtn(
+                    label: 'Continue with Google',
+                    labelColor: Colors.black,
+                    color: Colors.transparent,
+                  ),
+                ],
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class FlatBtn extends StatelessWidget {
+  final String label;
+  final Color? labelColor;
+  final Color? color;
+  final IconData? leading;
+  final FontWeight? fontWeight;
+  const FlatBtn(
+      {Key? key,
+      required this.label,
+      this.leading,
+      this.labelColor,
+      this.color, this.fontWeight})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: const [
+          BoxShadow(
+              blurRadius: .2, spreadRadius: .2, blurStyle: BlurStyle.outer)
+        ],
+      ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Icon(
+          leading,
+          color: labelColor,
+        ),
+        Text(
+          label,
+          style: TextStyle(color: labelColor, fontWeight: fontWeight),
+        ),
+      ]),
     );
   }
 }

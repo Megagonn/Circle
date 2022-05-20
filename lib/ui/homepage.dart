3d3e3080663dcb837bb99687fb3363b1b208d684
Meg <yusuffrasheed.yr@gmail.com>
2022-05-20 21:39:29 +0100

    commit

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twit/ui/newpost.dart';
 import 'package:twit/utilities/ui.utl.dart';
 
 class Home extends StatefulWidget {
@@ -14,29 +15,67 @@ class _HomeState extends State<Home> {
     return
         // const SliverSafeArea(
         //   sliver:
-       SafeArea(
-         child: Scaffold(
-             backgroundColor: Colors.white,
-             floatingActionButton: FloatingActionButton(onPressed: (){}, child: const Icon(Icons.add),),
-             bottomNavigationBar: BottomNavigationBar(items: [
-          BottomNavigationBarItem(icon: IconButton(onPressed: (){}, icon: const Icon(Icons.home_outlined, color: Colors.blueAccent,)), label: '',
-          // backgroundColor: Colors.amberAccent,
-          ),
-          BottomNavigationBarItem(icon: IconButton(onPressed: (){}, icon: const Icon(Icons.search_outlined, color: Colors.blueAccent,)), label: 'Home', ),
-          BottomNavigationBarItem(icon: IconButton(onPressed: (){}, icon: const Icon(Icons.notifications_outlined, color: Colors.blueAccent,)), label: 'Home'),
-          BottomNavigationBarItem(icon: IconButton(onPressed: (){}, icon: const Icon(Icons.mail_outline_outlined, color: Colors.blueAccent,)), label: 'Home'),
-             ],
-             elevation: 8,
-             ),
-             body: CustomScrollView(
+        SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const Post()));
+          },
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.home_outlined,
+                    color: Colors.blueAccent,
+                  )),
+              label: '',
+              // backgroundColor: Colors.amberAccent,
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search_outlined,
+                    color: Colors.blueAccent,
+                  )),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+                icon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.notifications_outlined,
+                      color: Colors.blueAccent,
+                    )),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.mail_outline_outlined,
+                      color: Colors.blueAccent,
+                    )),
+                label: 'Home'),
+          ],
+          elevation: 8,
+        ),
+        body: CustomScrollView(
           slivers: [
-             SliverAppBar(
+            SliverAppBar(
               elevation: 1,
               floating: true,
               backgroundColor: Colors.white,
               leading: const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: CircleAvatar(radius: 14,),
+                child: CircleAvatar(
+                  radius: 14,
+                ),
               ),
               centerTitle: true,
               title: const Icon(
@@ -46,30 +85,29 @@ class _HomeState extends State<Home> {
               pinned: false,
               snap: true,
               actions: [
-                IconButton(onPressed: (){}, icon: const Icon(Icons.star_outlined, color: Colors.blueAccent))
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.star_outlined,
+                        color: Colors.blueAccent))
               ],
             ),
-            
             SliverList(
-              delegate: SliverChildListDelegate(
-               [
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 const CircleCard(),
-                 
-               ]
-              ))
+                delegate: SliverChildListDelegate([
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+              const CircleCard(),
+            ]))
           ],
-             ),
-           ),
-       );
+        ),
+      ),
+    );
     // );
   }
 }
